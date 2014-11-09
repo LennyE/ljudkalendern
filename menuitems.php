c27b376a9547a6fead8b2ab7190c4fa52829152d
Lenny Ekberg <Lenny@Lennys-MacBook-Pro.local>
2014-11-09 16:01:37 +0100

    Design changes
    Updates for 2014 calendar.

---
+++ menuitems.php
@@ -0,0 +1,18 @@
+		<div id="mainnav" class="sixteen columns center"><!--center in class-->
+            <span class="feature-image">
+            	<a href="index.php">
+            		<img style="max-width:250px;" src="images/logo/logo.png" alt="logo">
+            	</a>
+            </span>
+			
+
+			<ul>
+				<li class="link"><a href="index.php">2014</a></li>
+				<li class="link"><a href="2013.php">2013</a></li>
+				<li class="link"><a href="#">2012</a></li>
+				<!-- <li class="link"><a href="spotify://spotify:album:37xl28qGX45H01dsex3nUx">test</a></li> -->
+			</ul>
+
+		</div>
+		
+		
